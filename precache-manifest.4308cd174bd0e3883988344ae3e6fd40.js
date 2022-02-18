4bc42064a611a950b7a624f3b249862f7d48d4b4
splndr <qqwwa@yandex.ru>
2022-02-18 17:05:03 +0300

    Updates

---
+++ precache-manifest.4308cd174bd0e3883988344ae3e6fd40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b55a9eb54a944cb2b6e185c3a2c8ab",
+    "url": "/funkjus/hasura/index.html"
+  },
+  {
+    "revision": "78d27501462b9921aea0",
+    "url": "/funkjus/hasura/static/css/main.d7e95554.chunk.css"
+  },
+  {
+    "revision": "935defe0e704ba23e1f4",
+    "url": "/funkjus/hasura/static/js/2.553eac01.chunk.js"
+  },
+  {
+    "revision": "78d27501462b9921aea0",
+    "url": "/funkjus/hasura/static/js/main.fb984441.chunk.js"
+  },
+  {
+    "revision": "4bf998380a4a9e09f435",
+    "url": "/funkjus/hasura/static/js/runtime~main.c955e0e5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/funkjus/hasura/static/media/logo.06e73328.svg"
+  }
+]);
